8eeeccc361cc16ee279ab696a95b54b1174b3a49
borshon420 <borshonarafat420@gmail.com>
2021-09-28 17:05:04 +0600

    react-bootstrap-explore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
+  const items = [
+    {name: 'first item', description: 'habi jabi gubi kobi khabi pabi', img: 'https://avatars.githubusercontent.com/u/6853419?s=200&v=4'},
+    {name: 'first item', description: 'habi jabi gubi kobi khabi pabi', img: 'https://avatars.githubusercontent.com/u/6853419?s=200&v=4'},
+    {name: 'first item', description: 'habi jabi gubi kobi khabi pabi', img: 'https://avatars.githubusercontent.com/u/6853419?s=200&v=4'},
+    {name: 'first item', description: 'habi jabi gubi kobi khabi pabi', img: 'https://avatars.githubusercontent.com/u/6853419?s=200&v=4'}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+        {
+          items.map(item => <Card item={item}></Card>)
+        }
     </div>
+    
   );
 }
+
+/*  <div className="App">
+       <h1>React ola Ami</h1>
+       <button type="button" className="btn btn-primary">Primary</button>
+           <button type="button" className="btn btn-secondary">Secondary</button>
+           <button type="button" className="btn btn-success">Success</button>
+           <button type="button" className="btn btn-danger">Danger</button>
+           <button type="button" className="btn btn-warning">Warning</button>
+           <button type="button" className="btn btn-info">Info</button>
+           <button type="button" className="btn btn-light">Light</button>
+           <button type="button" className="btn btn-dark">Dark</button>
+           <button type="button" className="btn btn-link">Link</button>
+     </div> */
 
 export default App;
